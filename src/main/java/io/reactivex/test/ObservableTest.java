f81c91e13a3402532dd38a71cf40061985782d3f
xiong.lei <xiong.lei@ubtrobot.com>
2019-05-13 20:14:13 +0800

    first commit

---
+++ src/main/java/io/reactivex/test/ObservableTest.java
@@ -0,0 +1,15 @@
+package io.reactivex.test;
+
+/**
+ * author : lei
+ * e-mail : xiong.lei@ubtrobot.com
+ * date   : 2019/5/13 19:44
+ * desc   :
+ * version: 1.0
+ */
+public class ObservableTest {
+    public static final void main(String[] args){
+        System.out.println("hehhhh");
+
+    }
+}
